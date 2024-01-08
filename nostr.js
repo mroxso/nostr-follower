cb0da2b3724b554a97cfbaf99d977774928f653e
mroxso <24775431+mroxso@users.noreply.github.com>
2024-01-08 21:39:23 +0100

    display as list item and add function for input text

--- nostr.js
+++ nostr.js
@@ -6,31 +6,48 @@ let counter = 0;
 
 let pCounter = document.getElementById('counter');
 let inputPubkey = document.getElementById('inputPubkey');
-// pubkey = inputPubkey.value;
+let ulCardList = document.getElementById('cardList');
+
+inputPubkey.addEventListener('input', function() {
+        pubkey = this.value;
+        getFollower().catch(console.error);
+});
 
 async function getFollower() {
-    const pool = new NostrTools.SimplePool()
+        ulCardList.innerHTML = '';
+        counter = 0;
+        pCounter.innerHTML = "Counter: " + counter;
+        
+        const pool = new NostrTools.SimplePool()
 
-    let h = pool.subscribeMany(
-        [...relays],
-        [
-          {
-            kinds: [3],
-            '#p': [pubkey]
-            // authors: [pubkey]
-          },
-        ],
-        {
-          onevent(event) {
-            console.log(event)
-            counter++;
-            pCounter.innerHTML = "Counter: " + counter;
-          },
-          oneose() {
-            h.close()
-          }
-        }
-      )
+        let h = pool.subscribeMany(
+                [...relays],
+                [
+                    {
+                        kinds: [3],
+                        '#p': [pubkey]
+                        // authors: [pubkey]
+                    },
+                ],
+                {
+                    onevent(event) {
+                        var date = new Date(event.created_at * 1000);
+
+                        // console.log(event)
+                        
+                        counter++;
+                        pCounter.innerHTML = "Counter: " + counter;
+                        let li = document.createElement('li');
+                        li.classList.add('list-group-item');
+                        li.id = event.pubkey;
+                        li.innerHTML = event.pubkey + " - " + date.toLocaleString();
+                        ulCardList.appendChild(li);
+                    },
+                    oneose() {
+                        h.close()
+                    }
+                }
+            )
 }
 
 getFollower().catch(console.error);
